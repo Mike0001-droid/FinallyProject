81c85c841e9c61bda56ae5632df3e5fba6c2bd40
Mikhail <majorov-2004@list.ru>
2025-05-07 23:50:38 +0500

    Добавил автоматическую документацию и зависимости

--- finally_project/finally_project/urls.py
+++ finally_project/finally_project/urls.py
@@ -2,11 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 from account.views import MyTokenObtainPairView
 from rest_framework_simplejwt.views import TokenRefreshView
+from django.conf import settings
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/users/user/', include('account.urls', namespace='user')),
-    path('api/users/token/create/', MyTokenObtainPairView.as_view(), name='token_auth'),
-    path('api/users/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('users/', include('account.urls', namespace='user')),
+    path('token/create/', MyTokenObtainPairView.as_view(), name='token_auth'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+
+if settings.DEBUG:
+    from rest_framework.documentation import include_docs_urls
+    urlpatterns.append(path('api/', include_docs_urls(title='API docs')))
