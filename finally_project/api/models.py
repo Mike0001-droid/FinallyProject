6cb96faa6884574d5bce768e8b3f2cd13bcd2eb2
Mikhail <majorov-2004@list.ru>
2025-05-08 07:58:13 +0500

    Создаль модель Task

--- finally_project/api/models.py
+++ finally_project/api/models.py
@@ -1,10 +1,21 @@
 from django.db import models
+from account.models import MyUser
+from django.utils import timezone
 
+
+STATUS_CHOICES = (
+    ("Открыто", "Открыто"), 
+    ("В работе", "В работе"), 
+    ("Выполнено", "Выполнено"),
+) 
 
 class Task(models.Model):
-    pass
-# Создание задач руководителем
-# Назначение исполнителя
-# Описание, дедлайн, статус
-# Комментарии (упрощённый чат внутри задачи)
-# Статусы: открыто, в работе, выполнено
+    name = models.CharField("Название задачи", max_length=100)
+    description = models.TextField("Описание задачи", max_length=500)
+    executor = models.OneToOneField(MyUser, on_delete=models.CASCADE)
+    deadline = models.DateTimeField("Дата регистрации")
+    comments = models.TextField("Комментарии", max_length=500)
+    status = models.CharField("Статус", choices=STATUS_CHOICES, max_length=9, default='Открыто')
+
+    def __str__(self):
+        return f"{self.executor.last_name} - {self.name}"
